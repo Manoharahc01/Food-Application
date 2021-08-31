4292b1a31fa8220f42287c67644e0feb7bbb1125
Manohar <manoharmany99@gmail.com>
2021-08-31 13:29:52 -0700

    created database and credential configuration files

---
+++ configs/database.js
@@ -0,0 +1,15 @@
+const properties = require('./properties');
+const MongoClient = require('mongodb').MongoClient;
+
+module.exports.connect = function (database) {
+    MongoClient.connect(properties.db.uri, (error, client) => {
+        if (error) {
+            console.log(`not connected to ${properties.db.dbName} database`);
+            database(null);
+        } else {
+            console.log(`connected to ${properties.db.dbName} database`);
+            var db = client.db(properties.db.dbName);
+            database(db);
+        }
+    });
+}
---
+++ configs/properties.js
@@ -0,0 +1,9 @@
+module.exports = {
+    db: {
+        uri: "mogodb://localhost:27017/",
+        dbName: "Food_Delicious"
+    },
+    collection: {
+        user_master: "userMaster"
+    }
+}
